b96dc8d7338d083281e38e543cf3a746c27c9b7d
PrantoMondal <prantomondal517@gmail.com>
2023-08-08 00:48:22 +0600

    test get requea

--- server/index.js
+++ server/index.js
@@ -2,10 +2,21 @@ console.log("Hello World");
 const express = require('express');
 //import express package
 const PORT = 3000;
-
-//Creating an API
 const app = express();
-app.listen(PORT,"0.0.0.0",function(){
+//Creating an API
+// http://<yourIpAddress>/helloWorld
+app.get('/hello-world',(req, res)=>{
+    res.json({hi:"hello world"});
+})
+
+app.get("/",(req, res)=>{
+res.json({name:"Peanto Mondal"})
+})
+
+
+//GET, PUT, POST, DELETE, UPDATE -> CRUD
+
+app.listen(PORT,function(){
 console.log(`Connected at port${PORT}`)});
 //localhost
 //127.0.0.1
