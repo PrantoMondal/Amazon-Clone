c8012db347c2da0a00d09b328024c1821f76b313
PrantoMondal <prantomondal517@gmail.com>
2023-08-14 01:43:47 +0600

    auth done

--- lib/constants/global_variabls.dart
+++ lib/constants/global_variabls.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+String uri = 'http://IP:3000';
+
 class GlobalVariables {
   // COLORS
   static const appBarGradient = LinearGradient(
--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -1,22 +1,45 @@
+import 'package:amazon_clone/constants/error_handling.dart';
+import 'package:amazon_clone/constants/global_variabls.dart';
 import 'package:amazon_clone/models/user.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
+import '../../../constants/utils.dart';
 
 class AuthService {
   //sign up user
   void signUpUser({
+    required BuildContext context,
     required String email,
     required String password,
     required String name,
   }) async {
     try {
       User user = User(
-          id: '',
-          name: name,
-          password: password,
-          address: '',
-          type: '',
-          token: '');
-      http.post(url);
-    } catch (e) {}
+        id: '',
+        name: name,
+        email: email,
+        password: password,
+        address: '',
+        type: '',
+        token: '',
+      );
+      http.Response res = await http.post(Uri.parse('$uri/api/signup'),
+          body: user.toJson(),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          });
+      //print(res.body);
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          showSnackBar(
+              context, 'Account Created! Login With the same credentials!');
+        },
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
   }
 }
